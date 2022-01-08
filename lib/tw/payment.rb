0af4cfffbb2b040dafe7601b4fe5422896f35692
willychang <a0903084107@gmail.com>
2022-01-08 13:09:49 +0800

    init

---
+++ lib/tw/payment.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "payment/version"
+
+module Tw
+  module Payment
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/tw/payment_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Tw::Payment do
+  it "has a version number" do
+    expect(Tw::Payment::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
